fafe42e8be5afde68349fc0d7e98a54616c4d499
Radicals <cclose27@gmail.com>
2020-05-11 14:28:53 +1000

    Will now show profiles even without a profile pic

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#(:picture, :name, :sex, :age, :about_me, :work_education, :location, :height, :health, :vices, :pets, :children, :religion, :politics, :starsign, :looking_for, :orientation)
+
+Profile.create(name: 'Jerry', sex: 'Male', age: '33', about_me: 'hey! I\'m a friendly guy who loves to party!', work_education: 'Bachelors of Comedy', location: 'Australia', height: '175', health: 'I exercise a few days per week', vices: 'I don\'t drink or smoke', pets: 'I have a pet', children: 'I don\'t have any children but I\'m flexible', religion: 'Orthodox', politics: 'Centrist', starsign: 'Leo', looking_for: 'Something casual', orientation: 'Straight')
