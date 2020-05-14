ff7a9fa9121e6dbfd408765e3b518d19639bed08
Radicals <cclose27@gmail.com>
2020-05-14 13:22:48 +1000

    basic name search implemented and working

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,12 @@
 class Profile < ApplicationRecord
     has_one_attached :picture
     belongs_to :user
+
+    def self.search(search)
+        if search
+            where(["name LIKE ?", "%#{search}%"])
+        else
+            all
+        end
+    end
 end
